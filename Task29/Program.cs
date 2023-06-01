873869c5b70ea98e99dd4c08361e88a1b49b01ae
Kikot Ekaterina <kati-kik@yandex.ru>
2023-06-01 10:02:49 +0300

    решена задача 29

---
+++ Task29/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+array[0] = 1;
+array[1] = 2;
+array[2] = 5;
+array[3] = 7;
+array[4] = 19;
+array[5] = 6;
+array[6] = 1;
+array[7] = 33;
+Console.WriteLine($"Результат: [{string.Join(", ", array)}]");
